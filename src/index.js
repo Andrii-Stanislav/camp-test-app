3f2fed0c1bc3a8c1d3d6dffc7f42a7efe24e99e9
Andrii Stanislav <66518828+Andrii-Stanislav@users.noreply.github.com>
2024-05-22 20:45:41 +0200

    add router basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/camp-test-app">
       <Provider store={store}>
         <App />
       </Provider>
